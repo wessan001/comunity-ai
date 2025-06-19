920b133c2e8b2cbe057b2a0776d2724bf64f9ed8
wessan001 <wesleyortegassan@gmail.com>
2025-06-19 13:55:38 -0300

    Correções

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import LoginPage from "@/app/login/page";
+import { Inter, Roboto_Mono } from "next/font/google"
+import LoginPage from "@/components/LoginPage"
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export default function Home() {
   return (
-   <div>
-    < LoginPage />
-   </div>
-  );
+    <div className={`${geistSans.variable} ${geistMono.variable}`}>
+      <LoginPage />
+    </div>
+  )
 }
