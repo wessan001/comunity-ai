6742cbf95c6a8e6d2d1234f24bc32c491797a114
wessan001 <wesleyortegassan@gmail.com>
2025-06-20 08:54:36 -0300

    Adição Dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="text-white min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-3xl font-bold">Bem-vindo à Dashboard</h1>
+    </div>
+  )
+}
